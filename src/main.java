af1e8952eaa9a0d65a80fbe4f5c8d14f0581ed3e
Büşra ÇİÇEK <37375743+bussracicekk@users.noreply.github.com>
2019-07-15 12:56:32 +0300

    Update main.java

--- src/main.java
+++ src/main.java
@@ -19,7 +19,7 @@ public class main {
 	public static Window w;
 	
 	public static void main(String[] args) {
-		JFrame frame = new JFrame("Application on CS-TECH by Busra Cicek");
+		JFrame frame = new JFrame("CS-TECH");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Container contentPane = frame.getContentPane();
 		JPanel buttonsPanel = new JPanel();
@@ -28,37 +28,37 @@ public class main {
 		textPane.setEditable(false);
 		JScrollPane scrollPane = new JScrollPane(textPane);
 		contentPane.add(scrollPane, BorderLayout.CENTER);
-		com = new computer(4, 9);
-		computer = new computer();
+		com = new computer(0, 9);
+		computer = new computer();//computer can random numbers
 		
-		w = new Window("", 800, 400);
-		user = (user) new user(10, 200,9)
+		w = new Window(800, 400);//main window
+		user = (user) new user(10, 200,9)//user class is invoked with this
 		.setbind(new bind.AlertListener() {
 			@Override
 			public void onAlert(String output) {
 				
-				String userGuessResult = computer.hintcomputer(output); 
+				String userGuessResult = computer.hintcomputer(output); //guesses of user
 				guess.addToHistory(output + " [" + userGuessResult + "]");
 				if(userGuessResult.equals("+4-0")) {
-					comPad.setMessage(" CONGRUTILATIONS, YOU WON !!!");
+					comPad.setMessage(" CONGRUTILATIONS, YOU WON !!!");//When user is won
 					user.setVisibility(InterFace.unvisible);
 					return;
 				}
-				comPad.computerGuess(com.guess()); // take ai's guess about user's number and display it on screen
+				comPad.computerGuess(com.guess());
 				hint.setVisibility(InterFace.visible);
 				user.setVisibility(InterFace.unvisible);
 			}
 		})
 		.setVisibility(InterFace.unvisible);
 		
-		w.addInterFace(user);
+		w.addInterFace(user);//user interface
 		
 		hint = (UserHint) new UserHint(50, 280)
 		.setbind(new bind.AlertListener() {
 			@Override
 			public void onAlert(String output) {
 				if(output.equals("40")) {
-					comPad.setMessage("Computer WON !!!");
+					comPad.setMessage("Computer WON !!!");//When computer is won
 					hint.setVisibility(InterFace.unvisible);
 				}else {
 					comPad.setMessage("LET'S GUESS MY NUMBER : ");
@@ -68,7 +68,7 @@ public class main {
 		})
 		.setVisibility(InterFace.unvisible);
 		
-		w.addInterFace(hint);
+		w.addInterFace(hint);//hint interface
 		comPad = (ComPad) new ComPad(20, 40)
 		.setbind(new bind.AlertListener() {
 			@Override
@@ -91,12 +91,13 @@ public class main {
 			}
 		});
 				
-		w.addInterFace(menu);
+		w.addInterFace(menu);//menu interface
 		guess = (Guesses) new Guesses(630, 50)
 		.setbind(new bind.AlertListener() {
 			@Override
 			public void onAlert(String output) {
 				if(output.equals("reset")) {
+					//Reset of datas
 					guess.reset(false);
 					comPad.reset(false);
 					user.reset(false);
@@ -108,7 +109,7 @@ public class main {
 		})
 		.setVisibility(InterFace.unvisible);
 						
-		w.addInterFace(guess);
+		w.addInterFace(guess);//guess interface
 	}
 
 	
